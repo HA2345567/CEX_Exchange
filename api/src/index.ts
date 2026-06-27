d3ff9fb98a53a68f7f88593c60a042055249f3af
My Name <bhardwajharsh0312@gmail.com>
2026-06-28 01:08:09 +0530

    completed api routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from  'cors';
 import { orderRouter } from './routes/order';
-
+import { depthRouter } from './routes/depth';
+import { tradesRouter } from './routes/trades';
+import { klineRouter } from './routes/kline';
+import { tickerRouter } from './routes/ticker';
 
 
 const app = express();
@@ -9,6 +12,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/v1/order',orderRouter);
+app.use('/api/v1/depth',depthRouter);
+app.use('/api/v1/trades',tradesRouter)
+app.use('/api/v1/ticker',tickerRouter);
+app.use('/api/v1/kline',klineRouter);
 
 
 app.listen(3000,()=>{
--- api/src/routes/kline.ts
+++ api/src/routes/kline.ts
@@ -18,6 +18,34 @@ klineRouter.get("/",async(req ,res)=>{
 
     let query;
     switch(interval){
+        case '1m':
+            query = `SELECT * FROM klines_1m WHERE bucket >= $1 AND bucket <= $2`;
+            break;
+        case '1h':
+            query = `SELECT * FROM  klnes_1h WHERE bucket >= $1 AND bucket <= $2 `;
+            break;
+        case '1w':
+            query = `SELECT * FROM klines_1w WHERE bucket >= $1 AND bucket <= $2`;
+            break;
+            default :
+            return res.status(400).send('Invalid interval');
         
     }
+     try {
+        const result = await pgClient.query(query,[new Date(startTime * 1000 as unknown as string), new Date(endTime * 1000 as unknown as string)]);
+        res.json(result.rows.map(x => ({
+            close: x.close,
+            end: x.bucket,
+            high: x.high,
+            low:x.low,
+            open: x.open,
+            quoteVolume: x.quoteVolume,
+            start: x.start,
+            tardes: x.trades,
+            volume: x.volume,
+        })))
+     }catch(e){
+        console.log(e);
+        res.status(500).send(e);
+     }
 })
